573a5c8e15db6d5f1852f8b4b00f572bda3b594c
Kristin Whetstone <kwhetstone@cloudbees.com>
2017-09-11 10:54:30 -0400

    Tests for Reminder
    Also stripped out the piece about checking for the licensing screens
    since this no longer nags with the dropdown.

---
+++ src/test/java/com/cloudbees/jenkins/plugins/advisor/ReminderTest.java
@@ -0,0 +1,78 @@
+package com.cloudbees.jenkins.plugins.advisor;
+
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.JenkinsRule.WebClient;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+import com.google.gson.Gson;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ReminderTest {
+
+  @Rule
+  public JenkinsRule j = new JenkinsRule();
+
+  private String blurb;
+
+
+  @Before
+  public void setup() throws Exception {
+      Properties props = new Properties();
+      InputStream input = Reminder.class.getResourceAsStream("Reminder/message.properties");
+      props.load(input);
+      blurb = props.getProperty("Blurb");
+  }
+
+  @Test
+  public void visitConfigPage() throws Exception {
+      // just in case the disable test is run first
+      j.getPluginManager().getPlugin(AdvisorGlobalConfiguration.PLUGIN_NAME).enable();
+
+      // page displays the warning
+      WebClient w = j.createWebClient();
+      String part = AdvisorGlobalConfiguration.getInstance().getUrlName();
+      
+      HtmlPage managePage = w.goTo("manage");
+      assertTrue(managePage.asText().contains(blurb));
+
+      // page doesn't show warning
+      submitForm(w, part, "test@test.test", "testPassword", false);
+      managePage = w.goTo("manage");
+      assertFalse(managePage.asText().contains(blurb));
+
+      // page shows warning
+      submitForm(w, part, "", "", false);
+      managePage = w.goTo("manage");
+      assertTrue(managePage.asText().contains(blurb));
+
+      // page doesn't show warning
+      submitForm(w, part, "", "", true);
+      managePage = w.goTo("manage");
+      assertFalse(managePage.asText().contains(blurb));
+  }
+
+  @Test
+  public void testPluginDisabled() throws Exception {
+      j.getPluginManager().getPlugin(AdvisorGlobalConfiguration.PLUGIN_NAME).disable();
+
+      WebClient w = j.createWebClient();
+      HtmlPage managePage = w.goTo("manage");
+      assertFalse(managePage.asText().contains(blurb));
+  }
+
+  private void submitForm(WebClient wc, String part, String userEmail, String userPassword, boolean nagOff) throws Exception {
+      HtmlForm form = (HtmlForm) (wc.goTo(part).getFirstByXPath("//form[@action='configure']"));
+      form.getInputByName("_.email").setValueAttribute(userEmail);
+      form.getInputByName("_.password").setValueAttribute(userPassword);
+      form.getInputByName("_.nagDisabled").setChecked(nagOff);
+      j.submit(form);
+  }
+
+}
