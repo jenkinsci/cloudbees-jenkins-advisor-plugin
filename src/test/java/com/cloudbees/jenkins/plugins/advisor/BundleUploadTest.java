93d3a20d74a56779d484be11561eda42fcc55548
Kristin Whetstone <kwhetstone@cloudbees.com>
2017-12-07 14:38:20 -0500

    Remove invalid messaging around "updating credentials"

--- src/test/java/com/cloudbees/jenkins/plugins/advisor/BundleUploadTest.java
+++ src/test/java/com/cloudbees/jenkins/plugins/advisor/BundleUploadTest.java
@@ -119,8 +119,6 @@ public class BundleUploadTest {
     wireMockRule.shutdownServer();
 
     subject.run();
-
-    //assertThat(getTestCapturedLog(), containsString("SEVERE:"));
   }
 
   @WithoutJenkins
